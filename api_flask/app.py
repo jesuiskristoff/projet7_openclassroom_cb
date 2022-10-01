5a89bd62b6f62c4174e40848a11b739be5cb98ca
jesuiskristoff <jesuiskristoff@gmail.com>
2022-10-01 19:37:25 +0200

    Redone all modelisations steps with interpretability to take into account ALL the customers Adding Flask API

---
+++ api_flask/app.py
@@ -0,0 +1,189 @@
+import pandas as pd
+import flask
+from flask import Flask, request, jsonify
+import pickle
+from sklearn.preprocessing import StandardScaler
+
+print("Flask Version: ", flask.__version__)
+
+# Initialisation de l'application Flask
+app = Flask(__name__)
+# Chargement des données de configuration
+app.config.from_pyfile('config.py')
+
+
+# Fonction permettant de charger un fichier pickle
+def load(filename, filepath='./'):
+    with open(f'{filepath}/{filename}.pickle', 'rb') as f:
+        return pickle.load(f)
+
+
+# Chargement des données des clients, du modèle et de l'explainer SHAP
+data = load(app.config["DATA_FILENAME"], f'../{app.config["DATA_LOCATION"]}')
+
+# Remarque sur les données clients : dans un cas réel d'un environnement de production, les données seraient bien
+# entendu stockées dans une base de données ou sous forme de fichier crypté dans un endroit sécurisé pour être conforme
+# à la RGPD et garantir la protection des données des clients. Ici, les données sont stockées au même endoit que
+# l'application.
+
+pipeline = load(app.config["MODEL_FILENAME"], f'../{app.config["MODEL_LOCATION"]}')
+explainer = load(app.config["EXPLAINER_FILENAME"], f'../{app.config["MODEL_LOCATION"]}')
+
+# Traitement des données pour les préparer à leur utilisation dans les différents contextes de l'application
+data.index = data['SK_ID_CURR']
+customer_ids = list(data.index)
+data_customers = data[[c for c in data.columns if c not in ['TARGET', 'SK_ID_CURR']]]
+scaler = StandardScaler()
+scaled_data_customers = pd.DataFrame(scaler.fit_transform(data_customers),
+                                     columns=data_customers.columns,
+                                     index=data_customers.index)
+
+# Récupération du modèle dans le pipeline sauvegardé
+model = pipeline.steps[1][1]
+
+
+# Classe héritant de Exception qui sera raise en cas de comportement inattendu de l'application
+# Cette classe aura comme arguments le status de l'erreur survenue, un message associé et des données le cas échéant
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, data=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.data = data
+
+    def to_dict(self):
+        rv = dict(self.data or ())
+        rv['erreur'] = self.message
+        return rv
+
+
+# Classe permettant de structurer la réponse d'une requête en format json
+class Response:
+
+    def __init__(self, message, data, status_code=200):
+        self.json = jsonify({"message": message, "data": data})
+        self.json.status_code = status_code
+
+
+# Route permettant de récupérer les données concernant les clients.
+# Cette route prend pour paramètre "id_" qui peut valoir:
+# - soit -1 : dans ce cas la route renvoie la liste des identifiants de tous les clients
+# - soit l'identifiant du client : dans ce cas la route renvoie les données du client demandé
+# Si l'id donné n'existe pas, alors la requête renverra une erreur 404.
+@app.route('/customer/<id_>', methods=['GET'])
+def customer_controller(id_: int):
+    id_ = int(id_)
+
+    if id_ not in customer_ids + [-1]:
+        raise InvalidUsage(message="Le client n'a pas été trouvé.", status_code=404)
+
+    if id_ == -1:  # on veut récupérer la liste des clients
+        return Response(message="Identifiants de tous les clients récupérés.", status_code=200, data=customer_ids).json
+    else:  # on veut récupérer les données d'un client
+        customer_data = data_customers[data_customers.index == id_].T.to_dict()
+        return Response(message="Données du client récupérées.", status_code=200, data=customer_data).json
+
+
+# Route permettant de prédire si un client va rembourser son prêt.
+# Cette route prend pour paramètre "id_" qui correspond à l'identifiant du client.
+# Un argument de requête "threshold" doit être obligatoirement ajouté. Il s'agit du seuil de tolérance à appliquer.
+# Si l'id donné n'existe pas, alors la requête renverra une erreur 404.
+# Si le "threshold" n'est pas présent alors la requête renverra une erreur 400.
+@app.route('/predict/<id_>', methods=['GET'])
+def predict_controller(id_: int):
+    id_ = int(id_)
+
+    if id_ not in customer_ids:
+        raise InvalidUsage(message="Le client n'a pas été trouvé.", status_code=404)
+
+    customer_data = scaled_data_customers[scaled_data_customers.index == id_]
+    threshold = request.args.get('threshold')
+
+    if threshold is None:
+        raise InvalidUsage(message=f"Le paramètre `threshold` est absent de la requête.", status_code=400)
+
+    threshold = float(threshold)
+
+    y_probability = model.predict_proba(customer_data)[:, 1][0]
+    y_predict = int((y_probability > threshold) * 1)
+
+    return Response(message=f"La prédiction a été réalisée avec succès.",
+                    status_code=200,
+                    data={"id_": id_,
+                          "threshold": threshold,
+                          "predict": y_predict,  # renvoie la décision d'accorder le prêt ou non
+                          "probability": y_probability  # renvoie la probabilité de ne pas rembourser le prêt
+                          }).json
+
+
+# Route permettant de réaliser l'interprétabilité globale ou locale pour le modèle choisi.
+# Cette route prend pour paramètre "id_" qui peut valoir:
+# # - soit -1 : dans ce cas la route se charge de l'interprétabilité globale sur un échantillon de clients,
+# # - soit l'identifiant du client : dans ce cas la route se charge de l'interprétabilité locale du client demandé.
+# Un argument de requête "n_customers" doit être obligatoirement ajouté. Il de la taille de l'échantillon sur lequel
+# effectuer l'interprétabilité globale.
+# Si l'id donné n'existe pas, alors la requête renverra une erreur 404.
+# Si le "n_customers" n'est pas présent alors la requête renverra une erreur 400.
+# Si le "n_customers" est plus élevé que la taille maximale des données alors la requêtre renverra une erreur 500.
+@app.route('/interp/<id_>', methods=['GET'])
+def interp_controller(id_):
+    id_ = int(id_)
+
+    if id_ not in customer_ids + [-1]:
+        raise InvalidUsage(message="Le client n'a pas été trouvé.", status_code=404)
+
+    if id_ == -1:  # interprétabilité globale
+
+        n_customers = request.args.get('n_customers')
+
+        if n_customers is None:
+            raise InvalidUsage(message=f"Le paramètre `n_customers` est absent de la requête.", status_code=400)
+
+        n_customers = int(n_customers)
+
+        try:
+            sampled_data = scaled_data_customers.sample(n=n_customers, random_state=42)
+        except ValueError as e:
+            raise InvalidUsage(message=f"Quelque chose s'est mal passé: {e}.", status_code=500)
+
+        interp_data = {
+            "interp_data": sampled_data.to_dict(),  # renvoie les données pour l'interprétabilité globale
+            "shap_values": explainer.shap_values(sampled_data, check_additivity=False).tolist(),  # renvoie les valeurs de Shapely
+            "expected_value": explainer.expected_value  # renvoie la valeur de base
+        }
+
+        return Response(message="Données nécessaires à l'interprétabilité globale récupérées.",
+                        status_code=200,
+                        data=interp_data).json
+
+    else:  # interprétabilité locale
+
+        customer_data = scaled_data_customers[scaled_data_customers.index == id_]
+
+        interp_data = {
+            "id_": id_,
+            "interp_data": customer_data.to_dict(),  # renvoie les données pour l'interprétabilité locale
+            "shap_values": explainer.shap_values(customer_data, check_additivity=False).tolist(),  # renvoie les valeurs de Shapely
+            "expected_value": explainer.expected_value  # renvoie la valeur de base
+        }
+
+        return Response(message="Données nécessaires à l'interprétabilité locale récupérées.",
+                        status_code=200,
+                        data=interp_data).json
+
+
+# Cette partie permet d'attraper les erreurs renvoyées à l'intérieur des différents controlleurs pour les convertir en
+# réponse adaptée en format json
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+# Lancement de l'application Flask sur le port contenu dans le fichier de configuration
+if __name__ == "__main__":
+    app.run(debug=False, port=app.config["PORT"])
---
+++ api_flask/config.py
@@ -0,0 +1,6 @@
+PORT = 3000
+DATA_LOCATION = 'cleaning_filtering_modelisation/data'
+DATA_FILENAME = 'data_filtered_all_customers'
+MODEL_LOCATION = 'cleaning_filtering_modelisation/models'
+MODEL_FILENAME = 'model'
+EXPLAINER_FILENAME = 'explainer'
