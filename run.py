43bd3b25cc9087a287abfa7edfd4445527ac3ec5
jesuiskristoff <jesuiskristoff@gmail.com>
2022-10-02 19:31:33 +0200

    adding port to app.run in run.py

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 # Importation de l'application Flask depuis le dossier api_flask
 
 from api_flask.app import app
+import os
 
 # Lancement de l'application Flask
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port, debug=False)
